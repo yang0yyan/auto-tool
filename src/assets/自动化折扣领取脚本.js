7934176ec56b8aba31f62a641691b2e4ff697dca
yangyang <58495156+yang0yyan@users.noreply.github.com>
2026-06-03 17:45:28 +0800

    Refactor discount script with enhancements and fixes

--- src/assets/自动化折扣领取脚本.js
+++ src/assets/自动化折扣领取脚本.js
@@ -13,12 +13,17 @@
     'use strict';
 
     // Your code here...
+    // 折扣阈值
     const discountThreshold = 8.5;
     let discountIndex = 0;
-    let isStop = false;
+    // 启停标志
+    let isStop = true;
+    let blacklist = []
     // 添加快捷键
     document.addEventListener('keydown', function(event) {
         if (event.altKey && event.key === '1') {
+            if(!isStop) return
+            isStop = false;
             optionSync(); // alt + 1
         }else if(event.altKey && event.key === '2'){
             console.log("<<<<<<<<<<<<< 中止运行 <<<<<<<<<<<<<");
@@ -29,42 +34,61 @@
     async function optionSync() {
         console.log(">>>>>>>>>>>>> 开始运行 >>>>>>>>>>>>>");
         isStop = false;
+        blacklist = []
         discountIndex = 0;
-        let buttons = getAllBtn();
         // 遍历并点击按钮
-        for (let index = 0; index < buttons.length; index++) {
+        let index = 0;
+        while(true){
+            let buttons = getAllBtn();
+            if(index>=buttons.length) break
+            console.log("1", "0");
             if(isStop) return
-            discountIndex++;
             const btn = buttons[index];
+            // 如果是黑名单中的就跳过
+            if(blacklist.includes(btn.customizeId)) {
+                index++;
+                continue
+            }
+            discountIndex++;
             simulateClick(btn)
+            console.log("1", "1");
             if(isStop) return
             await chackDialogShow()
+            console.log("1", "2");
             if(isStop) return
-            await optionDiscount()
+            await optionDiscount(btn)
+            console.log("1", "3");
             if(isStop) return
-            await new Promise(r => setTimeout(r, 500));
+            await new Promise(r => setTimeout(r, 1000));
+            console.log("1", "4");
+            index = 0;
         }
+        isStop = true;
         console.log("<<<<<<<<<<<<< 运行结束 <<<<<<<<<<<<<");
     }
     // 获取所有“获取”按钮
     function getAllBtn() {
-        // 获取table体
-        const body = document.querySelector(".TB_hiddenScrollBar_5-154-0")
-        if (!body) return [];
-
-        const btnList = body.querySelectorAll(".BTN_outerWrapper_5-154-0")
+        const trList = document.querySelectorAll(".TB_tr_5-154-0")
+        const ids = document.querySelectorAll(".TableList_lightText__YhSG2")
         // 获取所有领取按钮
         let getterList = []
-        for (let index = 0; index < btnList.length; index++) {
-            const element = btnList[index];
-            if (element.innerHTML.indexOf('领取')>-1) {
-                getterList.push(element)
+        for (let index = 1; index < trList.length; index++) {
+            const tr = trList[index];
+            const btnList = tr.querySelectorAll(".BTN_outerWrapper_5-154-0")
+            for (let index2 = 0; index2 < btnList.length; index2++) {
+                const element = btnList[index2];
+                if (element.innerHTML.indexOf('领取')>-1) {
+                    // 获取唯一键 id，并关联到 element 上
+                    const idElement = ids[index-1];
+                    element.customizeId = getID(idElement.innerText)
+                    getterList.push(element)
+                }
             }
         }
         return getterList;
     }
     // 处理折扣的函数
-    async function optionDiscount(){
+    async function optionDiscount(element){
         const body = getDialogBody();
         const isTure = chackDiscount(body);
         if(isTure){
@@ -72,14 +96,21 @@
             await new Promise(r => setTimeout(r, 2000));
             if(isStop) return
             // closeDialog()
+            // await chackDialogHide()
             const btn = searchButton("确认提交", body)
             simulateClick(btn)
+            console.log("1");
             await chackDialogHide()
+            console.log("2");
             await new Promise(r => setTimeout(r, 1000));
+            console.log("3");
             closeDialog()
+            console.log("4");
             await chackDialogHide()
+            console.log("5");
         }else{
             console.log("折扣不通过", discountIndex)
+            blacklist.push(element.customizeId)
             await new Promise(r => setTimeout(r, 2000));
             closeDialog()
             await chackDialogHide()
@@ -109,8 +140,8 @@
         while(true){
             const body = getDialogBody();
             if(!body) break
+            closeDialog()
             await new Promise(r => setTimeout(r, 200));
-            closeDialog();
         }
     }
     // 等待窗口显示
@@ -124,7 +155,8 @@
     // 关闭窗口
     function closeDialog(){
         const cBtn = document.querySelector(".MDL_iconWrapper_5-154-0")
-        simulateClick(cBtn)
+        if(cBtn)
+            simulateClick(cBtn)
     }
     // -------------------------------窗口相关----------------------------- end
     // 模拟点击
@@ -158,5 +190,10 @@
             }
         }
         return null
+    }
+    // 获取文本中的数字
+    function getID(str){
+        const matches = str.match(/\d+/g);
+        return matches[0]
     }
 })();
